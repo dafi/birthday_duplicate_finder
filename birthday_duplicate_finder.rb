b1ac6408d0aab82712d58e70e70abf12c8318a2d
Davide Ficano <davide.ficano@gmail.com>
2016-03-13 11:55:00 +0100

    First commit

---
+++ birthday_duplicate_finder.rb
@@ -0,0 +1,33 @@
+def read_bdays(bday_csv_path)
+    map = {}
+
+    open(bday_csv_path).read.gsub(/\r\n?/, "\n").each_line do |line|
+        (id, name, bday, blog) = line.split(';')
+        list = map[bday]
+        if list == nil
+            list = []
+            map[bday] = list
+        end
+        list.push(name)
+    end
+
+    return map.delete_if { |k,v| v.size == 1}
+end
+
+def find_similar(map)
+    map.each do |bday,list|
+        for i in 0..list.count-1
+            iwords = list[i].split(/[ -]/)
+            for j in (i + 1)..list.count-1
+                jwords = list[j].split(/[ -]/)
+                arr = iwords & jwords
+                if !arr.empty?
+                    puts "#{bday} '#{list[i]}' with '#{list[j]}' = #{arr}"
+                end
+            end
+        end
+    end
+end
+
+map = read_bdays(ARGV[0])
+find_similar(map)
