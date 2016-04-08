9036ee42e7588477a49bfa9c59fdd1b7dbb288cf
Davide Ficano <davide.ficano@gmail.com>
2016-04-08 17:11:25 +0200

    Better command line parameters error handling

--- birthday_duplicate_finder.rb
+++ birthday_duplicate_finder.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 def read_bdays(bday_csv_path)
     map = {}
 
@@ -27,6 +28,11 @@ def find_similar(map)
             end
         end
     end
+end
+
+if ARGV.empty?
+  puts 'Specify the path to birthday CSV file'
+  exit
 end
 
 map = read_bdays(ARGV[0])
